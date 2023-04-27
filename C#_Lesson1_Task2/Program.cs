e9f854e44c0b090b695a4f0d23ee2f23d86e0f71
Василий Сторожев <vvstorozhev@bk.ru>
2023-04-27 13:41:30 +0300

    added 2 solved tasks

---
+++ C#_Lesson1_Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число: ");
+float A = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+float B = float.Parse(Console.ReadLine());
+
+if(A>B)
+{   
+    Console.Write("Число ");
+    Console.Write(A);
+    Console.Write(" больше числа ");
+    Console.Write(B);
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(B);
+    Console.Write(" больше числа ");
+    Console.Write(A);    
+}
---
+++ C#_Lesson1_Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Привет! Введите первое число: ");
+float A = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+float B = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+float C = float.Parse(Console.ReadLine());
+
+float max = A;
+
+
+if(A > max) max = A;
+if(B > max) max = B;
+if(C > max) max = C;
+
+Console.Write("Максимальное число: ");
+Console.Write(max);
