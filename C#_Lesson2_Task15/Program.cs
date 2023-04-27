5c9d13af47c7e64d5c47eacb17ca15ed0fea7242
Василий Сторожев <vvstorozhev@bk.ru>
2023-04-27 23:56:50 +0300

    Tasks by Lesson 2 added

---
+++ C#_Lesson2_Task15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Привет, введи число: ");
+int arg1 = int.Parse(Console.ReadLine());
+
+if(arg1 > 0 && arg1 < 8)
+{
+    if(arg1 > 5 && arg1 < 8) Console.WriteLine("Крутяк, выходные!");
+    if(arg1 > 0 && arg1 < 6) Console.WriteLine("Кажется пора на работу :с ");
+}
+else Console.WriteLine("Введенное число не похоже на день недели :с ");
