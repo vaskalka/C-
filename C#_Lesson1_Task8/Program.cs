97bada9e746a9321de231c78967756349f7939ec
Василий Сторожев <vvstorozhev@bk.ru>
2023-04-27 15:47:51 +0300

    Home tasks added

---
+++ C#_Lesson1_Task8/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+*/
+
+Console.WriteLine("Привет, введи число, а я выведу список четных от 1 до твоего числа: ");
+float A = float.Parse(Console.ReadLine());
+Console.Write("Твоё число: ");
+Console.WriteLine(A);
+
+Console.WriteLine("А вот последовательность четных чисел от 1, до твоего числа: ");
+
+int count = 0;
+float remainder, B = A;
+
+if(A > 0)
+{
+    while(count < Math.Abs(A)) 
+    {   
+        remainder = B % 2;
+        if(remainder == 0) Console.WriteLine(B);
+        count++;
+        B--; 
+    } 
+}
+else
+{
+    while(count < Math.Abs(A)) 
+    {   
+        remainder = B % 2;
+        if(remainder == 0) Console.WriteLine(B);
+        count++;
+        B++; 
+    }     
+}
