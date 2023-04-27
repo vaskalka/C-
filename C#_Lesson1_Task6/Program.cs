aa3f56daee1f0fa75325ccfa78fd182826cbd835
Василий Сторожев <vvstorozhev@bk.ru>
2023-04-27 14:01:21 +0300

    Task3 solved

---
+++ C#_Lesson1_Task6/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Привет! Введи число, проверим его на четность: ");
+float A = float.Parse(Console.ReadLine());
+
+float remainder = A % 2;
+Console.WriteLine(remainder);
+
+if(remainder == 0) 
+{
+    Console.Write("Число четное, остаток равен: ");
+    Console.Write(remainder);  
+}
+else
+{
+    Console.Write("Число нечетное, остаток равен: ");
+    Console.Write(remainder);      
+}
+
