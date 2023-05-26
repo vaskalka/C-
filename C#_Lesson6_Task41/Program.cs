64d219b8e871769a33233e4b75c3d841a2464a31
Василий Сторожев <vvstorozhev@bk.ru>
2023-05-26 20:15:25 +0300

    Lesson 6, solved tasks added

---
+++ C#_Lesson6_Task41/Program.cs
@@ -0,0 +1,54 @@
+﻿void fillArray(int[] collection, int numOfNumber)
+{
+    Console.WriteLine($"Число вводимых чисел - {numOfNumber}");
+
+    int index = 0;
+    int remains = numOfNumber;
+
+    for (int i = 0; i < numOfNumber; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        collection[i] = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Число под индексом [{i}] = {collection[i]} ");
+        remains--;
+        Console.WriteLine($"Осталось ввести чисел {remains}");
+    }
+    Console.WriteLine("Ввод окончен!");
+}
+
+
+void printArray(int[] collection, int numOfNumber)
+{
+    Console.WriteLine("Ваш массив: ");
+    for (int i = 0; i < numOfNumber; i++)
+    {
+        Console.Write($"{collection[i]} ");
+    }
+}
+
+
+void checkMoreThenZero(int[] collection, int numOfNumber)
+{
+    int nubOfChecked = 0;
+    for (int i = 0; i < numOfNumber; i++)
+    {
+        if (collection[i] > 0)
+        {
+            nubOfChecked++;
+        }
+    }
+    Console.WriteLine($"Количество чисел в массиве больше нуля = {nubOfChecked}");
+}
+
+
+
+
+
+Console.WriteLine("Сколько чисел вы хотите ввести? - ");
+int M = int.Parse(Console.ReadLine());
+
+int[] array = new int[M];
+
+fillArray(array, M);
+printArray(array, M);
+checkMoreThenZero(array, M);
---
+++ C#_Lesson6_Task43/Program.cs
@@ -0,0 +1,28 @@
+﻿void mathdoubleersection (double k1, double b1, double k2, double b2)
+{
+    double check = k1 - k2;
+    if (check == 0) 
+    {
+        Console.WriteLine("Прямые имеют одинаковый угол наклона, пересечения нет");        
+    }
+    else
+    {
+        double x = (b2 - b1)/(k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Точка пересечения y = {y}, x = {x}");        
+    }
+}
+
+Console.WriteLine("Введите значения для первой прямой: ");
+Console.Write("Введите k1 = ");
+double k1 = double.Parse(Console.ReadLine());
+Console.Write("Введите b1 = ");
+double b1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значения для второй прямой: ");
+Console.Write("Введите k2 = ");
+double k2 = double.Parse(Console.ReadLine());
+Console.Write("Введите b2 = ");
+double b2 = double.Parse(Console.ReadLine());
+
+mathdoubleersection(k1, b1, k2, b2);
