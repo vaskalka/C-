b4d18bedcef8986cf063eee400e543a0d5fd6668
Василий Сторожев <vvstorozhev@bk.ru>
2023-06-04 19:29:16 +0300

    project realased with function NewArrayLessOrEquallyThreeSimbols

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -9,20 +9,33 @@
 }
 
 
-string PrintArray(string[] Array, int ArrayLength)
+void PrintArray(string[] Array, int ArrayLength)
 {
     Console.WriteLine("Ваш массив: ");
     for(int i = 0; i < ArrayLength; i++)
     {
         Console.Write($"{Array[i]} ");
     }
+    Console.WriteLine();
 }
 
 
-// string NewArrayLessOrEquallyThreeSimbols()
-// {
-
-// }
+void NewArrayLessOrEquallyThreeSimbols(string[] Array, int ArrayLength)
+{
+    string[] NewArray = new string[ArrayLength];
+    int Counter = 0;
+    for(int i = 0; i < ArrayLength; i++)
+    {
+        if(Array[i].Length <= 3)
+        {
+            NewArray[Counter] = Array[i];
+            Counter++;
+        }
+    }    
+    Console.WriteLine("Отсортированный массив по кол-ву символов в элементе: ");
+    PrintArray(NewArray, ArrayLength);
+    Console.WriteLine($"Кол-во элементов <= 3: {Counter}");
+}
 
 Console.WriteLine("Введите количество символов массива: ");
 int ArrayLength = int.Parse(Console.ReadLine());
@@ -30,3 +43,4 @@ string[] Array = new string[ArrayLength];
 
 FillArray(Array, ArrayLength);
 PrintArray(Array, ArrayLength);
+NewArrayLessOrEquallyThreeSimbols(Array, ArrayLength);
