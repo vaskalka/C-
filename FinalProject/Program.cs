f6bc34c63b480d7d2d2c7c477adf6c7cc644d296
Василий Сторожев <vvstorozhev@bk.ru>
2023-06-04 16:49:39 +0300

    2 methods fill and print array added

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿void FillArray(string[] Array, int ArrayLength)
+{
+    Console.WriteLine("Введите элементы массива: ");
+    for(int i = 0; i < ArrayLength; i++)
+    {
+        Array[i] = Console.ReadLine();
+        Console.WriteLine($"Осталось ввести элементов: {ArrayLength -i - 1}");
+    }
+}
+
+
+string PrintArray(string[] Array, int ArrayLength)
+{
+    Console.WriteLine("Ваш массив: ");
+    for(int i = 0; i < ArrayLength; i++)
+    {
+        Console.Write($"{Array[i]} ");
+    }
+}
+
+
+// string NewArrayLessOrEquallyThreeSimbols()
+// {
+
+// }
+
+Console.WriteLine("Введите количество символов массива: ");
+int ArrayLength = int.Parse(Console.ReadLine());
+string[] Array = new string[ArrayLength];
+
+FillArray(Array, ArrayLength);
+PrintArray(Array, ArrayLength);
